46d215b374e42f1d8171517cb2fd492b843ff23a
Maximilian Fischer <github@maaeps.de>
2023-03-04 14:33:59 +0100

    Initial commit

---
+++ auth_example_server/lib/server.dart
@@ -0,0 +1,14 @@
+import 'package:serverpod/serverpod.dart';
+
+import 'src/generated/protocol.dart';
+import 'src/generated/endpoints.dart';
+
+void run(List<String> args) async {
+  final pod = Serverpod(
+    args,
+    Protocol(),
+    Endpoints(),
+  );
+
+  await pod.start();
+}
---
+++ auth_example_server/setup-tables.cmd
@@ -0,0 +1,22 @@
+@echo off
+
+@REM Start docker
+echo Starting docker
+docker compose up --build --detach
+
+:LOOP
+netstat -o -n -a | >nul findstr "8090" && (
+    echo Waiting for Postgres...
+    timeout /t 2 /nobreak > NUL
+    goto :PORT_FOUND
+)
+echo Waiting for Postgres...
+goto :LOOP
+
+:PORT_FOUND
+echo Postgres is ready
+type .\generated\tables-serverpod.pgsql | docker compose run -T postgres env PGPASSWORD="QWd7prfCZVePOddbb6w74GZwzVIZ7uuk" psql -h postgres -U postgres -d auth_example
+echo Stopping docker
+docker compose stop
+
+exit /b
